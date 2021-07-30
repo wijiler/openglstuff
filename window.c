784b32d70c357ce930a03b580c5ff114a68f3d57
wij <41879253+wijiler@users.noreply.github.com>
2021-07-30 19:48:09 -0400

    korega requiem da.

--- window.c
+++ window.c
@@ -6,14 +6,26 @@ void render(void);
 int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA); // sets values like I want depth and color
-  glutInitWindowPosition(100,100);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowPosition(100,100); 
   glutInitWindowSize(640,480);
-  glutCreateWindow("editor or smth"); // window title
+  glutCreateWindow("editor or smth");
   
-  glutDisplayFunc(render); // tells opengl hey stinky head render is the render function
-  glutMainLoop(); // will make it not crash
+  glutDisplayFunc(render);
+  glutMainLoop();
 }
 
 void render(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glBegin(GL_TRIANGLES);
+       glColor3f(1, 0 , 0);
+       glVertex2f(-0.5, -0.5);
+       glColor3f(0, 1 , 0);
+       glVertex2f(0.5, -0.5);
+       glColor3f(0, 0 , 1);
+       glVertex2f(0.0, 0.5); 
+    glEnd();
+
+    glutSwapBuffers();
 }
