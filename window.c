d41a7b7630bbad5fa92aee4f4b615be2e45b1b51
wij <41879253+wijiler@users.noreply.github.com>
2021-07-30 19:28:23 -0400

    so I forgot to comment on stuff

--- window.c
+++ window.c
@@ -6,13 +6,13 @@ void render(void);
 int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowPosition(100,100); 
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA); // sets values like I want depth and color
+  glutInitWindowPosition(100,100);
   glutInitWindowSize(640,480);
-  glutCreateWindow("editor or smth");
+  glutCreateWindow("editor or smth"); // window title
   
-  glutDisplayFunc(render);
-  glutMainLoop();
+  glutDisplayFunc(render); // tells opengl hey stinky head render is the render function
+  glutMainLoop(); // will make it not crash
 }
 
 void render(void) {
